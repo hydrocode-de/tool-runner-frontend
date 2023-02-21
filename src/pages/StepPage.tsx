c2e31b6aa65ce06bfd050a008e77aff5a1ee713d
Mirko Mälicke <mirko@hydrocode.de>
2023-02-21 14:54:04 +0100

    slimming step-page

--- src/pages/StepPage.tsx
+++ src/pages/StepPage.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 import { RouteComponentProps } from "react-router";
 
 import { getStep } from '../backend'
+import ErrorMessage from "../components/ErrorMessage";
+import LoadingScreen from "../components/LoadingScreen";
 import StepDetail from "../components/StepDetail";
 
 const StepPage: React.FC<RouteComponentProps<{toolName?: string, stepName: string, action?: string}>> = ({ match }) => {
@@ -24,41 +26,12 @@ const StepPage: React.FC<RouteComponentProps<{toolName?: string, stepName: strin
         
     }, [match.params.stepName])
 
-    // loading
-    if (status === 'loading') {
-        return (
-            <IonGrid style={{height: '100%'}}>
-                    <IonRow style={{height: '100%'}}>
-                        <IonCol className="ion-align-items-center ion-justify-content-center" style={{display: 'flex'}}>
-                            <div style={{display: 'flex', flexDirection: 'column'}} className="ion-align-items-center">
-                                <IonSpinner name="crescent" style={{fontSize: '5rem'}} />
-                                <IonNote style={{marginTop: '0.6rem'}}>The results are loaded from the backend...</IonNote>
-                            </div>
-                        </IonCol>
-                    </IonRow>
-            </IonGrid>
-        )
-    }
-
-    // error rendering
-    if (error != '' || !step) {
-        return <>
-            <IonCard color="danger">
-                <IonCardHeader>
-                    <IonCardTitle>Backend error occured</IonCardTitle>
-                    <IonCardContent>
-                        { error }
-                    </IonCardContent>
-                    <IonButton color="light" fill="clear" routerLink="/" routerDirection="root">BACK</IonButton>
-                </IonCardHeader>
-            </IonCard>
-        </>
-    }
-
     return (
         <IonPage>
             <IonContent fullscreen>
-                <StepDetail step={step} />
+                { status === 'loading' ? <LoadingScreen message="The results are loaded from the backend..." /> : null }
+                { status === 'finished' && error !== '' ? <ErrorMessage message={error}/> : null }
+                { status === 'finished' && step ?  <StepDetail step={step} /> : null }
             </IonContent>
         </IonPage>
     )
